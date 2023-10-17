29f014bad0b4d85cc2394bfa00050a3d6519a1bb
Bata Munkhbold <batamun@gmail.com>
2023-10-17 04:03:49 -0400

    Removed console.log

--- src/Table.js
+++ src/Table.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table';
 
 export const Table = ({ data, columns, options }) => {
-    console.log(data, columns, options)
+    
     const table = useReactTable({
         data,
         columns,
